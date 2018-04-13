ea439d14183441f95866cf70da4d20c52a191c8f
Zhu Tao <zhutao.iscas@gmail.com>
2018-04-13 22:12:19 +0800

    level2 p1

--- foobar/level2/solution.py
+++ foobar/level2/solution.py
@@ -1,21 +1,26 @@
 import math
 
+# the tricky part is: the remaining number might also fit a henchman
 
 def answer(total_lambs):
     # generous handout is a geometric sequence(q=2), Sn
     # stingy handout is a Fib sequence
-    if total_lambs in [0, 1, 2]:
-        return 0
-    generous_total = int(math.log(total_lambs+1, 2))
+    # for geometric sequence
 
-    F1 = 1
-    F2 = 1
-    sum_stingy = 2
-    stingy_total = 2
-    while True:
-        F1, F2 = F2, F1+F2
-        sum_stingy += F2
-        if sum_stingy > total_lambs:
-            break
+    generous_total = int(math.log(total_lambs+1, 2))
+    last = int(2 ** (generous_total - 1))
+    last2 = int(2 ** (generous_total - 2))
+    left = total_lambs - (2 * last - 1)
+    if left >= (last + last2):
+        generous_total += 1
+
+    # for fib sequence, sum(F(n)) = F(n+2) - 1
+    phi = (math.sqrt(5) + 1) / 2
+    stingy_total = int(math.log((total_lambs + 1) * math.sqrt(5) + .5, phi)) - 2
+
+    last = int(phi ** stingy_total / math.sqrt(5) + .5)
+    last2 = int(phi ** stingy_total / math.sqrt(5) + .5)
+    left = total_lambs - (int(phi ** (stingy_total + 2) / math.sqrt(5) + .5) - 1)
+    if left >= (last + last2):
         stingy_total += 1
     return stingy_total - generous_total
--- foobar/level2/test.py
+++ foobar/level2/test.py
@@ -1,6 +1,9 @@
 from solution import answer
 
 
-points = [0, 1, 2, 3, 4, 5, 100, 1000, 10000, 100000, 1000000]
+points = list(range(1, 200)) + [1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10, 143]
+#points = range(1, 10**6+1)
 for p in points:
-    print "answerr for {} is: {}".format(p, answer(p))
+    print "answer for {} is: {}".format(p, answer(p))
+    #if answer(p) != answer2(p):
+    #    print "doesn't equal for {}".format(p)
